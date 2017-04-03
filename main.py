e5e6c76edb04d3ab7570bea596de5d244a40a0d3
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-03 00:45:56 -0400

    window will enlarge or shrink as the fonts change; disable resizable funct

--- main.py
+++ main.py
@@ -4,51 +4,51 @@ from tkFont import Font
 
 
 class App():
-
     def __init__(self, root):
         self.root = root  # creates an instance --> what we call the root window
         self.main_panel = MainPanel(self.root)
         self.upper_bar = UpperBar(self.root, self.main_panel)
-        self.slider = Slider(self.root, self.main_panel, 10)
+        self.slider = Slider(self.root, self.main_panel)
         self.menu = MenuBar(self.root, self.main_panel)
 
 
-
-
 class MainPanel():
-
     def __init__(self, root):
         self.root = root
-        self.custom_font = Font(family="courier", size=12)
+        self.custom_font = Font(family="Verdana", size=14)
         self.root.option_add("*Font", self.custom_font)
         self.root.title("iHart GUI Demo")
-        self.default_width = 300
-        self.default_height = 270
+        self.default_width = 320
+        self.default_height = 340
         self.ratio = 1
-        frame_width = int(self.default_width * self.ratio)
-        frame_height = int(self.default_height * self.ratio)
-        self.root.geometry("%dx%d" % (frame_width, frame_height))
+        self.frame_dimensions()
+        self.root.resizable(0, 0)
 
-        # board = Frame(root, height=frame_height, width=frame_width)
-        # self.board = Canvas(board, bd=1, bg="black")
-        #
-        # board.grid(row=1, column=1)
-        # self.board.grid(row=1, column=1)
+    def frame_dimensions(self):
+        self.frame_width = int(self.default_width * self.ratio)
+        self.frame_height = int(self.default_height * self.ratio)
+        self.root.geometry("%dx%d" % (self.frame_width, self.frame_height))
 
     def larger_font(self):
-        size = self.custom_font['size']
-        self.custom_font.configure(size=size + 2)
-        self.root.update_idletasks()
+        self.ratio = self.ratio * 1.1
+        if self.ratio <= 1.50:
+            size = self.custom_font['size']
+            self.custom_font.configure(size=size + 2)
+            self.frame_dimensions()
+            self.root.update_idletasks()
 
     def smaller_font(self):
-        size = self.custom_font['size']
-        self.custom_font.configure(size=size - 2)
-        self.root.update_idletasks()
+        self.ratio = self.ratio * 0.9
+        if self.ratio >= 0.65:
+            size = self.custom_font['size']
+            self.custom_font.configure(size=size - 2)
+            self.frame_dimensions()
+            self.root.update_idletasks()
 
     def change_font(self, n):
         courier12 = Font(family="courier", size=12)
         helv12 = Font(family="Helvetica", size=12)
-        times12 = Font(family="Times", size=12, )
+        times12 = Font(family="Times", size=12)
         if n == 1:
             self.root.option_add("*Font", courier12)
             # self.custom_font.config(family="courier")
@@ -64,26 +64,25 @@ class MainPanel():
 
 
 class UpperBar():
-
     def __init__(self, root, main_panel):
         self.root = root
         self.board = main_panel
         logo = PhotoImage(file="logo.gif")
-        logo = logo.subsample(10,10)
+        logo = logo.subsample(10, 10)
         logoLabel = Label(root, image=logo)
         logoLabel.grid()
         # Style().configure('green/black.TLabel', foreground='green', background='black')
+
         flip_frame = Frame(self.root)
         flip_frame.pack(side="top", fill="x", expand=False)
         # flip_frame.config(width=self.board.default_width, height=2)
-        flip_frame.grid(row=0, columnspan=10, sticky=W + E, padx=5, pady=5)
+        flip_frame.grid(row=0, columnspan=10, sticky=W + E, pady=5)
         flipButton = Button(flip_frame, text="Flip Horizontal")
         flipButton.pack()
 
 
 class Slider():
-
-    def __init__(self, root, main_panel, init_val):
+    def __init__(self, root, main_panel):
 
         self.root = root
         self.board = main_panel
@@ -91,21 +90,21 @@ class Slider():
         FaceVar = IntVar()
         FaceVar.set(1)
         MotionVar = BooleanVar()
-        
+
         checkbox_frame = Frame(self.root)
-        checkbox_frame.grid(row=2, column=0, columnspan=3, sticky=W + E + N + S)
+        checkbox_frame.grid(row=2, column=0, columnspan=2,
+                            sticky=W + E + S)  # WE - stretch it horizontally but leave it centered vertically.
 
         enableface_checkbox = Checkbutton(checkbox_frame, text="Enable Face", variable=FaceVar, onvalue=1, offvalue=0)
-        enablemotion_checkbox = Checkbutton(checkbox_frame, text="Enable Motion", variable=MotionVar, onvalue=True, offvalue=False)
-        enableface_checkbox.grid(row=0, column=0)
-        enablemotion_checkbox.grid(row=0, column=2)
+        enablemotion_checkbox = Checkbutton(checkbox_frame, text="Enable Motion", variable=MotionVar, onvalue=True,
+                                            offvalue=False)
+        enableface_checkbox.grid(row=0, column=0, sticky=N + S + W, padx=15)
+        enablemotion_checkbox.grid(row=0, column=2, sticky=N + S + E)
 
         bottom_frame = Frame(self.root)
-        bottom_frame.grid(row=3, column=0, columnspan=3, sticky=W + E + N + S)
+        bottom_frame.grid(row=3, column=0, columnspan=3, sticky=W + E + S)
 
         self.create_buttons(bottom_frame)
-
-
 
     def create_buttons(self, frame):
 
@@ -119,8 +118,6 @@ class Slider():
         self.decreases = [None for _ in range(num)]
         self.vars = [None for _ in range(num)]
 
-
-        
         for i in range(num):
             self.vars[i] = IntVar()
         initial = 10
@@ -131,37 +128,35 @@ class Slider():
             self.scales[r].set(self.inputs[r])
             self.increases[r] = Button(frame, text="+", command=lambda r=r: self.increase_this(r))
             self.decreases[r] = Button(frame, text="-", command=lambda r=r: self.decrease_this(r))
-            self.labels[r].grid(row=r+3, column=0, sticky = W)
-            self.decreases[r].grid(row=r+3, column=1, sticky = S)
-            self.scales[r].grid(row=r+3, column=2, sticky = N)
-            self.increases[r].grid(row=r+3, column=3, sticky = S)
-            initial -=1
+            self.labels[r].grid(row=r + 3, column=0, sticky=W + N, ipadx=10, ipady=15)
+            self.decreases[r].grid(row=r + 3, column=1, columnspan=3, sticky=N + S + E, ipady=13)
+            self.scales[r].grid(row=r + 3, column=4, sticky=N)
+            self.increases[r].grid(row=r + 3, column=5, columnspan=3, sticky=N + S + E, ipady=13)
+            initial -= 1
 
-        root.bind('<Left>', self.keyDecrease)
-        root.bind('<Right>', self.keyIncrease)
+        self.root.bind('<Left>', self.keyDecrease)
+        self.root.bind('<Right>', self.keyIncrease)
 
-    def keyIncrease(self,event):
+    def keyIncrease(self, event):
         self.increase_this(0)
 
     def keyDecrease(self, event):
         self.decrease_this(0)
 
-
-    def increase_this(self,i):
+    def increase_this(self, i):
         input = self.inputs[i]
-        if (input+1 <= self.to_value[i]):
-            self.inputs[i] = input+1
+        if (input + 1 <= self.to_value[i]):
+            self.inputs[i] = input + 1
         self.scales[i].set(self.inputs[i])
 
     def decrease_this(self, r):
         input = self.inputs[r]
-        if (input-1 >=0):
-            self.inputs[r] = input-1
+        if (input - 1 >= 0):
+            self.inputs[r] = input - 1
         self.scales[r].set(self.inputs[r])
 
 
 class MenuBar():
-
     def __init__(self, root, main_panel):
         self.root = root
         self.board = main_panel
